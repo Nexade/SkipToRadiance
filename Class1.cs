7b2722b602f6c2b767bafcbb09afbcd737b31c70
Nexade <88131558+Nexade@users.noreply.github.com>
2022-02-12 13:43:03 +1000

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using Modding;
+using System;
+using UnityEngine;
+using Unity;
+using JetBrains.Annotations;
+using SFCore;
+using HutongGames.PlayMaker;
+using HutongGames.PlayMaker.Actions;
+
+namespace SkipToRadiance
+{
+    public class SkipToRadiance : Mod, ITogglableMod
+    {
+        public override string GetVersion() => "1.0";
+
+        bool RadianceEncountered = false;
+        public override void Initialize()
+        {
+            ModHooks.OnEnableEnemyHook += ModHooks_OnEnableEnemyHook;
+        }
+
+        private bool ModHooks_OnEnableEnemyHook(GameObject obj, bool isAlreadyDead)
+        {
+            if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "Dream_Final_Boss")
+            {
+                RadianceEncountered = true;
+            }
+            if (obj.name.Contains("Hollow Knight Boss") && RadianceEncountered)
+            {
+                obj.LocateMyFSM("Phase Control").SetState("Set Phase 4");
+            }
+            return isAlreadyDead;
+        }
+        public void Unload()
+        {
+            ModHooks.OnEnableEnemyHook -= ModHooks_OnEnableEnemyHook;
+        }
+    }
+}
